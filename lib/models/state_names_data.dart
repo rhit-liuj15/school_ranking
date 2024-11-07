7d5d0791657e19d719ef1f9661658bd36065337f
Ethan Liu <liuj15@rose-hulman.edu>
2024-11-07 02:16:49 -0500

    Sample HTTP GET read

--- lib/models/state_names_data.dart
+++ lib/models/state_names_data.dart
@@ -6,9 +6,21 @@ class StateNamesData {
   static final StateNamesData instance = StateNamesData._privateConstructor();
 
   StateNamesData._privateConstructor() {
-		var url = Uri.https('10.0.0.48:4800', 'states');
-		var response = http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-		print(response);
+		var url = Uri.http('10.0.0.48:4800', 'states');
+		// var url = Uri.http('ethan02.us:4800', 'states');
+		()async{
+			try {
+				final response = await http.get(url);
+				// Flutter enforces CORS. The needed change is performed server-side.
+				if (response.statusCode == 200) {
+					print('Response data: ${response.body}');
+				} else {
+					print('Failed to load data. HTTP Status Code: ${response.statusCode}');
+				}
+			} catch (e) {
+				print('Error occurred: $e');
+			}
+		}();
 	}
 }
 
--- lib/pages/all_schools_page.dart
+++ lib/pages/all_schools_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:p01_final_project/components/filter_component.dart';
 import 'package:p01_final_project/components/sortable_school_table.dart';
 import 'package:p01_final_project/models/all_school_data.dart';
 import 'package:p01_final_project/models/school_data.dart';
-
 
 class AllSchoolsPage extends StatefulWidget {
   const AllSchoolsPage({super.key});
@@ -12,9 +12,9 @@ class AllSchoolsPage extends StatefulWidget {
 }
 
 class _AllSchoolsPageState extends State<AllSchoolsPage> {
-	
-	late final List<SchoolData> allSchools = AllSchoolData.instance.allSchools;
-  late final List<bool> selected = List<bool>.generate(allSchools.length, (int index) => false);
+  late final List<SchoolData> allSchools = AllSchoolData.instance.allSchools;
+  late final List<bool> selected =
+      List<bool>.generate(allSchools.length, (int index) => false);
 
   @override
   void initState() {
@@ -28,9 +28,17 @@ class _AllSchoolsPageState extends State<AllSchoolsPage> {
         title: const Text("School Ranking List"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-				child: SortableSchoolTable(),
-			),
+      body: const Center(
+				child: Padding(
+					padding: EdgeInsets.all(40.0),
+					child: Row(
+					  children: [
+					    FilterComponent(),
+					    Placeholder(),
+					    Expanded(child: SortableSchoolTable()),
+					  ],
+					),
+      )),
     );
   }
 }
